9ac1a26fcc1c4e71dd53d2eb76cd5a4dd53a9fd0
LiangHaoXiang <1013568397@qq.com>
2021-08-19 17:15:55 +0800

    Initial Commit

---
+++ OpenGL_Test30_TextRender/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test30_TextRender
+//
+//  Created by Haoxiang Liang on 2021/8/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
